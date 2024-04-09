aee3910200c67c0e89f099c3c507c38090f0e19c
Jay Lee <jaeho.lee@snu.ac.kr>
2024-04-09 22:22:39 +0900

    :zap: Use terser to minify more

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  build: {
+    minify: 'terser'
+  },
   plugins: [
     react(),
     legacy({
