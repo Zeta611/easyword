a730ec7bacee17f1a499ac1348891705ddc28481
Jay Lee <jaeho.lee@snu.ac.kr>
2024-04-09 20:30:58 +0900

    :wrench: Try CSS polyfill

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
   plugins: [
     react(),
     legacy({
-      targets: ['last 2 versions, not dead, > 0.2% in KR, iOS >= 12.5'],
       modernPolyfills: ['es.array.to-sorted']
     })
   ]
