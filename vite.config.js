ce46d8da5f4cd7fc7275b8bc94510891a68ec75b
Jay Lee <jaeho.lee@snu.ac.kr>
2024-08-14 15:19:28 +0900

    :heavy_plus_sign: Use `@vitejs/plugin-legacy` instead of `vite-plugin-legacy-swc`
    This solves the build release error

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
-import { defineConfig } from 'vite'
-import legacy from 'vite-plugin-legacy-swc'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import legacy from "@vitejs/plugin-legacy";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   build: {
-    minify: 'terser',
-    cssMinify: 'lightningcss',
+    minify: "terser",
+    cssMinify: "lightningcss",
     terserOptions: {
       compress: {
-        passes: 5
-      }
-    }
+        passes: 5,
+      },
+    },
   },
   plugins: [
     react(),
     legacy({
-      modernPolyfills: ['es.array.to-sorted']
-    })
-  ]
-})
+      modernPolyfills: ["es.array.to-sorted"],
+    }),
+  ],
+});
