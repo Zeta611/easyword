0ed689915694247a412b11a4f96a0403297add00
Jay Lee <jaeho.lee@snu.ac.kr>
2025-08-01 23:18:00 +0900

    :wrench: Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "easyword";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            bun
+          ];
+        };
+      }
+    );
+}
