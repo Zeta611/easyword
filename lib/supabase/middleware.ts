e20f0abac2bcd6bec150296367dd5468d35f13c3
Jay Lee <jaeho.lee@snu.ac.kr>
2025-08-12 20:05:52 +0900

    :fire: Remove unused path from middleware

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -40,11 +40,7 @@ export async function updateSession(request: NextRequest) {
   const { data } = await supabase.auth.getClaims();
   const user = data?.claims;
 
-  if (
-    !user &&
-    (request.nextUrl.pathname.startsWith("/profile") ||
-      request.nextUrl.pathname.startsWith("/add-jargon"))
-  ) {
+  if (!user && request.nextUrl.pathname.startsWith("/profile")) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
     url.pathname = "/auth/login";
